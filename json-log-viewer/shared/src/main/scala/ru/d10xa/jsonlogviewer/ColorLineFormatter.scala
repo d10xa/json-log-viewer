84f4442e8dd008b8cf011556b8e7bbe2c73f191e
Andrey Stolyarov <d10xa@mail.ru>
2023-10-17 22:10:58 +0300

    Colored otherAttributes (#8)

--- json-log-viewer/shared/src/main/scala/ru/d10xa/jsonlogviewer/ColorLineFormatter.scala
+++ json-log-viewer/shared/src/main/scala/ru/d10xa/jsonlogviewer/ColorLineFormatter.scala
@@ -1,21 +1,21 @@
 package ru.d10xa.jsonlogviewer
 
-import cats.syntax.all.*
 import fansi.ErrorMode.Strip
 import fansi.EscapeAttr
 import fansi.Str
 
 class ColorLineFormatter(c: Config) extends OutputLineFormatter:
+  private val strEmpty: Str = Str("")
   private val strSpace: Str = Str(" ")
-  extension (s: String)
-    def ansiStrip: Str = Str(s, Strip)
+  private val strNewLine: Str = Str("\n")
+  extension (s: String) def ansiStrip: Str = Str(s, Strip)
 
   def levelToColor(level: String): EscapeAttr =
     level match
-      case "DEBUG"   => fansi.Color.LightGray
+      case "DEBUG"            => fansi.Color.LightGray
       case "WARNING" | "WARN" => fansi.Color.Yellow
-      case "ERROR"   => fansi.Color.Red
-      case _         => fansi.Color.White
+      case "ERROR"            => fansi.Color.Red
+      case _                  => fansi.Color.White
 
   def strLevel(levelOpt: Option[String], colorAttr: EscapeAttr): Seq[Str] =
     levelOpt match
@@ -32,7 +32,7 @@ class ColorLineFormatter(c: Config) extends OutputLineFormatter:
     colorAttr: EscapeAttr
   ): Seq[Str] =
     stackTraceOpt match
-      case Some(s) => Str("\n") :: colorAttr(s.ansiStrip) :: Nil
+      case Some(s) => strNewLine :: colorAttr(s.ansiStrip) :: Nil
       case None    => Nil
 
   def strLoggerName(
@@ -40,54 +40,71 @@ class ColorLineFormatter(c: Config) extends OutputLineFormatter:
     colorAttr: EscapeAttr
   ): Seq[Str] =
     loggerNameOpt match
-      case Some(loggerName) => strSpace :: colorAttr(loggerName.ansiStrip) :: Nil
-      case None             => Nil
+      case Some(loggerName) =>
+        strSpace :: colorAttr(loggerName.ansiStrip) :: Nil
+      case None => Nil
 
   def strThreadName(
     threadNameOpt: Option[String],
     colorAttr: EscapeAttr
   ): Seq[Str] =
     threadNameOpt match
-      case Some(threadName) => strSpace :: colorAttr(s"[${threadName.ansiStrip}]") :: Nil
-      case None             => Nil
-
-  def mapAsString(m: Map[String, String]): String =
-    m.map { case (k, v) => s"$k:$v" }.mkString("{", ",", "}")
+      case Some(threadName) =>
+        strSpace :: colorAttr(s"[${threadName.ansiStrip}]") :: Nil
+      case None => Nil
 
   def strOtherAttributes(
     otherAttributes: Map[String, String],
-    colorAttr: EscapeAttr
+    needNewLine: Boolean
   ): Seq[Str] =
     otherAttributes match
       case m if m.isEmpty => Nil
       case m =>
-        val formattedAttrs = colorAttr(mapAsString(m).ansiStrip)
-        strSpace :: formattedAttrs :: Nil
+        val s = fansi.Str.join(
+          m.map { case (k, v) =>
+            fansi.Str.join(
+              Seq(
+                fansi.Color.Magenta(k.ansiStrip),
+                fansi.Color.LightMagenta(":"),
+                fansi.Color.Magenta(v.ansiStrip)
+              )
+            )
+          }.toSeq,
+          fansi.Color.LightMagenta("\n")
+        )
+        (if (needNewLine) strNewLine else strEmpty) :: s :: Nil
 
   def strPrefix(s: Option[String]): Seq[Str] =
     s match
-      case Some(prefix) => fansi.Color.White(prefix.ansiStrip) :: strSpace :: Nil
+      case Some(prefix) =>
+        fansi.Color.White(prefix.ansiStrip) :: strSpace :: Nil
       case None => Nil
 
   def strPostfix(s: Option[String]): Seq[Str] =
     s match
-      case Some(postfix) => strSpace :: fansi.Color.White(postfix.ansiStrip) :: Nil
+      case Some(postfix) =>
+        strSpace :: fansi.Color.White(postfix.ansiStrip) :: Nil
       case None => Nil
 
   override def formatLine(p: ParseResult): Str =
     p.parsed match
       case Some(line) =>
         val color = line.level.map(levelToColor).getOrElse(fansi.Color.White)
+        val substrings1 = Seq(
+          strPrefix(p.prefix),
+          Seq(fansi.Color.Green(line.timestamp.ansiStrip)),
+          strThreadName(line.threadName, color),
+          strLevel(line.level, color),
+          strLoggerName(line.loggerName, color),
+          strMessage(line.message, color),
+          strStackTrace(line.stackTrace, color)
+        ).flatten
         Str.join(
-          Seq(
-            strPrefix(p.prefix),
-            Seq(fansi.Color.Green(line.timestamp.ansiStrip)),
-            strThreadName(line.threadName, color),
-            strLevel(line.level, color),
-            strLoggerName(line.loggerName, color),
-            strMessage(line.message, color),
-            strStackTrace(line.stackTrace, color),
-            strOtherAttributes(line.otherAttributes, color),
+          substrings1 ++ Seq(
+            strOtherAttributes(
+              line.otherAttributes,
+              !substrings1.lastOption.exists(_.plainText.endsWith("\n"))
+            ),
             strPostfix(p.postfix)
           ).flatten
         )
