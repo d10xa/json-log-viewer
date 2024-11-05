ab67a5a25cb8e94bdb52c6e608f33a205d25bd56
Andrey Stolyarov <d10xa@mail.ru>
2024-11-05 20:35:06 +0300

    Fix exception `java.lang.StringIndexOutOfBoundsException: String index out of range: -1` (#12)

--- json-log-viewer/shared/src/main/scala/ru/d10xa/jsonlogviewer/JsonDetector.scala
+++ json-log-viewer/shared/src/main/scala/ru/d10xa/jsonlogviewer/JsonDetector.scala
@@ -7,14 +7,18 @@ class JsonDetector {
     @tailrec
     def loop(i: Int, start: Int, end: Int): (Int, Int) =
       val ri = s.length - i - 1
-      val rchar = s.charAt(ri)
-      val lchar = s.charAt(i)
-      val nextEnd = if(end == -1 && rchar == '}') ri else end
-      val nextStart = if(start == -1 && lchar == '{') i else start
-      if (s.length/2<i || (nextStart != -1 && nextEnd != -1)) {
-        (nextStart, nextEnd)
+      if (ri < 0) {
+        (-1, -1)
       } else {
-        loop(i + 1, nextStart, nextEnd)
+        val rchar = s.charAt(ri)
+        val lchar = s.charAt(i)
+        val nextEnd = if (end == -1 && rchar == '}') ri else end
+        val nextStart = if (start == -1 && lchar == '{') i else start
+        if (s.length / 2 < i || (nextStart != -1 && nextEnd != -1)) {
+          (nextStart, nextEnd)
+        } else {
+          loop(i + 1, nextStart, nextEnd)
+        }
       }
     val res = loop(0, -1, -1)
     res match
